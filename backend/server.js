7c7dc17b3d8479320263963102d4649cd4765b81
exploreIndia <exploreindia830@gmail.com>
2024-06-20 19:04:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,11 @@ const nodemailer = require("nodemailer");
 const bodyParser = require('body-parser');
 
 const app = express();
-app.get("/",(req,res)=>{
-  res.json("hello");
-})
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
@@ -53,3 +51,4 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+app.listen(5000, () => console.log("Server Running"));
